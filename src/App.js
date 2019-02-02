e7a78c04d7846d3c0313da11b3d4f66a6a64a843
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-02 15:59:20 +0530

    Added the Button Non-functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Button = (props) => {
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
+};
+
 class Game extends React.Component {
 	render() {
   	return (
     	<div>
       	<h3>Play Nine</h3>
+        <Button/>
       </div>
     );
   }
