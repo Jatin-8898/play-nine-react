d9d26c9c1d1bf189332150a95b2f44d3042ccc09
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-02 15:56:42 +0530

    Added the Game Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Game extends React.Component {
+	render() {
+  	return (
+    	<div>
+      	<h3>Play Nine</h3>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
      <div>
-       
+       <Game />
      </div> 
     );
   }
