7f4d943bff91455b99a95398fd1f4d0cb22bc6b7
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-02 19:45:44 +0530

    Added the ansIsCorrect state and checkAns fun and passed it to button compo

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ class Game extends React.Component {
 	state = {
 		selectedNumber: [],
 		randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+		answerIsCorrect: null,
 	};
 	selectNumber = (clickedNumber) => {
 		/* Handling the condition where same no gets added in the answer compo */
@@ -82,16 +83,25 @@ class Game extends React.Component {
 									 .filter(number => number !== clickedNumber)
 		}))
 	}
+	checkAnswer = () => {
+		this.setState( prevState =>  ({
+			/* Since we want the accumulated result of the numOfStar to the selectedNumber we use reduce */
+			answerIsCorrect:prevState.randomNumberOfStars === prevState.selectedNumber.reduce( (acc,n) => acc + n, 0)
+		}));
+	}
 	render() {
 		/* Destructuring the elements */
-		const { selectedNumber, randomNumberOfStars} = this.state;
+		const { selectedNumber, randomNumberOfStars, answerIsCorrect} = this.state;
 		return (
 			<div className="container-fluid text-center p-5">
 				<h1 className="text-success pb-4">Play Nine</h1>
 				<hr />
 				<div className="row">
 					<Stars numberOfStars={randomNumberOfStars}/>
-					<Button selectedNumber={selectedNumber}/>
+					<Button selectedNumber={selectedNumber}
+							checkAnswer={this.checkAnswer}
+							answerIsCorrect={answerIsCorrect}
+					/>
 					<Answer selectedNumber={selectedNumber}
 							unselectNumber={this.unselectNumber}
 					/>
