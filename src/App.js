4eb39c40faa25c1aec5c1d9b535f3ba87f53a0ce
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-02 17:43:35 +0530

    Used Lodash instead of Normal loop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import 'font-awesome/css/font-awesome.min.css';
 
 const Stars = (props) => {
 	const numberOfStars = 1 + Math.floor(Math.random()*9);
-	let stars = [];
+/* 	let stars = [];
 	for(let i=0; i<numberOfStars; i++){
 		stars.push(<i key={i} className="fa fa-star fa-2x"></i>);
-	}
+	} */
 
 	return (
 	  <div className="col-5">
-		{stars}
+		{_.range(numberOfStars).map(i =>
+        	<i key={i} className="fa fa-star fa-2x"></i>
+      	)}
 	  </div>
 	);
 };
